a1b4a090a53fe9c9d4439915c82faf6976ca810f
Diego <diegogplata@gmail.com>
2023-01-20 11:51:07 -0600

    add baja california sur files, reformat cdmx script

--- scripts/baja_california_sur.R
+++ scripts/baja_california_sur.R
@@ -6,64 +6,63 @@ library(raster)
 library(ggplot2)
 library(stars)
 library(tidyverse)
-library(sp)
 
 mex_population <- st_read("data/kontur_population_MX_20220630.gpkg")
 mxRDS <- readRDS("data/gadm36_MEX_1_sp.rds")
-df_state <- mxRDS %>% 
+state <- mxRDS %>% 
   st_as_sf() %>%
-  filter(NAME_1 == "Distrito Federal") |> 
+  filter(NAME_1 == "Baja California Sur") |> 
   st_transform(crs= st_crs(mex_population))
 
-df_state |>
+state |>
   ggplot() +
   geom_sf()
 
-df_population_state_intersection <- st_intersection(mex_population, df_state)
+population_state_intersection <- st_intersection(mex_population, state)
 
-df_bb <- st_bbox(df_population_state_intersection)
+bb <- st_bbox(population_state_intersection)
 
 
-df_bottom_left <- st_point(c(df_bb[["xmin"]], df_bb[["ymin"]])) |> 
+bottom_left <- st_point(c(bb[["xmin"]], bb[["ymin"]])) |> 
   st_sfc(crs = st_crs(mex_population))
 
-df_bottom_right <- st_point(c(df_bb[["xmax"]], df_bb[["ymin"]])) |> 
+bottom_right <- st_point(c(bb[["xmax"]], bb[["ymin"]])) |> 
   st_sfc(crs = st_crs(mex_population))
 
-df_state |> 
+state |> 
   ggplot() +
   geom_sf() +
-  geom_sf(data = df_bottom_left) +
-  geom_sf(data = df_bottom_right, color = "red")
+  geom_sf(data = bottom_left) +
+  geom_sf(data = bottom_right, color = "red")
 
-df_width <- st_distance(df_bottom_left, df_bottom_right)
+width <- st_distance(bottom_left, bottom_right)
 
-df_top_left <- st_point(c(df_bb[["xmin"]], df_bb[["ymax"]])) |> 
+top_left <- st_point(c(bb[["xmin"]], bb[["ymax"]])) |> 
   st_sfc(crs = st_crs(mex_population))
 
-df_height <- st_distance(df_bottom_left, df_top_left)
+height <- st_distance(bottom_left, top_left)
 
-if (df_width > df_height) {
-  df_w_ratio <- 1
-  df_h_ratio <- df_height / df_width
+if (width > height) {
+  w_ratio <- 1
+  h_ratio <- height / width
 } else {
-  df_h_ratio <- 1
-  df_w_ratio <- df_width / df_height
+  h_ratio <- 1
+  w_ratio <- width / height
 }
 
-df_size <- 1000
+size <- 1000
 
-nx <- floor(df_size * df_w_ratio)
-ny <- floor(df_size * df_h_ratio)
+nx <- floor(size * w_ratio)
+ny <- floor(size * h_ratio)
 
 # fix hardcoding
-df_state_rasterize <- st_rasterize(df_population_state_intersection, 
-                                nx = 902,
+state_rasterize <- st_rasterize(population_state_intersection, 
+                                nx = 550,
                                 ny = 1000)
 
-df_population_matrix <- matrix(df_state_rasterize$population, 
-                            nrow = floor(df_size * df_w_ratio),
-                            ncol = floor(df_size * df_h_ratio))
+population_matrix <- matrix(state_rasterize$population, 
+                            nrow = floor(size * w_ratio),
+                            ncol = floor(size * h_ratio))
 
 
 c1 <- met.brewer("VanGogh3")
@@ -72,14 +71,14 @@ swatchplot(c1)
 texture <- grDevices::colorRampPalette(c1, bias = 1)(256)
 swatchplot(texture)
 
-df_population_matrix |> 
+population_matrix |> 
   height_shade(texture = texture) |> 
-  plot_3d(heightmap = df_population_matrix,
+  plot_3d(heightmap = population_matrix,
           zscale = 100,
           solid = FALSE,
           shadowdepth = 0)
 
-png_outfile <- 'imgs/cdmx.png'
+png_outfile <- 'imgs/baja_california_sur.png'
 
 render_camera(theta = -20, phi = 45, zoom = .8)
 
