9b906deab19d7a4b8fd383971502a38d2e061edc
Diego <diegogplata@gmail.com>
2023-01-31 15:49:44 -0600

    annotation script for Guerrero

---
+++ annotation_scripts/annotate_guerrero.R
@@ -0,0 +1,42 @@
+library(magick)
+library(MetBrewer)
+library(colorspace)
+library(ggplot2)
+library(glue)
+library(stringr)
+
+img <- image_read("hd_imgs/guerrero.png")
+
+colors<- met.brewer("VanGogh3")
+swatchplot(colors)
+
+text_color <- darken(colors[7], .25)
+swatchplot(text_color)
+
+annot <- glue("Población estimada seccionada en hexágonos de 400 m") |>
+  str_wrap(60)
+
+img |> 
+  image_crop(gravity = "center",
+             geometry = "2000x2000+0-150") |> 
+  image_annotate("Densidad poblacional de Guerrero",
+                 gravity = "northwest",
+                 location = "+200+50",
+                 color = text_color,
+                 size = 100,
+                 weight = 700,
+                 font = "Futura") |> 
+  image_annotate(annot,
+                 gravity = "northwest",
+                 location = "+275+220",
+                 color = text_color,
+                 size = 50,
+                 font = "Futura") |> 
+  image_annotate(glue("Gráfica por Diego Plata (@foreverpelon) | ",
+                      "Data: Kontur Population 30 de junio de 2022"),
+                 gravity = "south",
+                 location = "+0+50",
+                 font = "Futura",
+                 color = alpha(text_color, .5),
+                 size = 35) |> 
+  image_write("annotated_hd_imgs/annotated_hd_guerrero.png")
--- scripts/guerrero.R
+++ scripts/guerrero.R
@@ -90,31 +90,37 @@ render_highquality(
 
 # higher quality render
 
-size <- 5000
+size <- 2000
 
-ags_rast <- st_rasterize(st_ags, 
-                         nx = floor(size * w_ratio),
-                         ny = floor(size * h_ratio))
+state_rasterize <- st_rasterize(population_state_intersection, 
+                                nx = floor(size * w_ratio),
+                                ny = floor(size * h_ratio))
 
-mat <- matrix(ags_rast$population, 
-              nrow = floor(size * w_ratio),
-              ncol = floor(size * h_ratio))
+population_matrix <- matrix(state_rasterize$population, 
+                            nrow = floor(size * w_ratio),
+                            ncol = floor(size * h_ratio))
 
-mat |> 
+c1 <- met.brewer("VanGogh3")
+swatchplot(c1)
+
+texture <- grDevices::colorRampPalette(c1, bias = 2)(256)
+swatchplot(texture)
+
+population_matrix |> 
   height_shade(texture = texture) |> 
-  plot_3d(heightmap = mat,
-          zscale = 100 / 5,
+  plot_3d(heightmap = population_matrix,
+          zscale = 100 / 2,
           solid = FALSE,
           shadowdepth = 0)
 
-render_camera(theta = -20, phi = 45, zoom = .8)
-
-
 {
+  png_outfile <- 'hd_imgs/guerrero.png'
+  render_camera(theta = -20, phi = 45, zoom = .8)
+  
   start_time <- Sys.time()
   cat(crayon::cyan(start_time), "\n")
   if (!file.exists(png_outfile)) {
-    png::writePNG(matrix(1), target = outfile)
+    png::writePNG(matrix(1), target = png_outfile)
   }
   render_highquality(
     filename = png_outfile,
@@ -123,12 +129,13 @@ render_camera(theta = -20, phi = 45, zoom = .8)
     lightaltitude = c(20, 80),
     lightcolor = c(c1[2], "white"),
     lightintensity = c(600, 100),
-    samples = 450,
-    width = 6000,
-    height = 6000
+    samples = 300,
+    width = 2000,
+    height = 2000
   )
   end_time <- Sys.time()
   diff <- end_time - start_time
   cat(crayon::cyan(diff), "\n")
-}
+  }
+
 
